c64d360e124a84d1283aee5410ed333ba97de0e9
imsk17 <imsumitkumar17@gmail.com>
2020-12-01 19:29:16 +0530

    :card_file_box: Add a DAO

---
+++ lib/db/gbu_dao.dart
@@ -0,0 +1,72 @@
+import 'package:GbuAgenda/models/school.dart';
+import 'package:GbuAgenda/models/section.dart';
+import 'package:GbuAgenda/models/subject.dart';
+import 'package:GbuAgenda/models/teacher.dart';
+import 'package:GbuAgenda/models/timetable.dart';
+import 'package:GbuAgenda/utils/constants.dart';
+import 'package:hive/hive.dart';
+
+import 'dao.dart';
+
+class GBUDao implements DAO {
+  @override
+  Future<List<School>> getAllSchools() async {
+    if (Hive.isBoxOpen(Constants.SchoolBox)) {
+      return Hive.box<School>(Constants.SchoolBox).values.toList();
+    } else {
+      return (await Hive.openBox(Constants.SchoolBox)).values.toList();
+    }
+  }
+
+  @override
+  Future<List<Section>> getAllSections(String school) async {
+    if (Hive.isBoxOpen(Constants.SchoolBox)) {
+      return Hive.box<Section>(Constants.SectionBox).values.toList();
+    } else {
+      return (await Hive.openBox<Section>(Constants.SectionBox))
+          .values
+          .toList();
+    }
+  }
+
+  @override
+  Future<Timetable> getTimetable(int section) async {
+    if (Hive.isBoxOpen(Constants.TimetableBox)) {
+      return Hive.box<Timetable>(Constants.TimetableBox).values.last;
+    } else {
+      return (await Hive.openBox<Timetable>(Constants.TimetableBox))
+          .values
+          .last;
+    }
+  }
+
+  @override
+  Future<Subject> getSubject(String code) async {
+    if (Hive.isBoxOpen(Constants.SubjectBox)) {
+      return Hive.box<Subject>(Constants.SubjectBox)
+          .values
+          .where((element) => element.subCode == code)
+          .first;
+    } else {
+      return (await Hive.openBox<Subject>(Constants.SubjectBox))
+          .values
+          .where((element) => element.subCode == code)
+          .first;
+    }
+  }
+
+  @override
+  Future<Teacher> getTeacher(int id) async {
+    if (Hive.isBoxOpen(Constants.TeacherBox)) {
+      return Hive.box<Teacher>(Constants.TeacherBox)
+          .values
+          .where((element) => element.id == id)
+          .first;
+    } else {
+      return (await Hive.openBox<Teacher>(Constants.TeacherBox))
+          .values
+          .where((element) => element.id == id)
+          .first;
+    }
+  }
+}
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,8 @@
 abstract class Constants {
   static const baseUrl = 'https://gbuagenda.herokuapp.com/';
+  static const SchoolBox = 'SchoolBox';
+  static const SectionBox = 'SectionBox';
+  static const TimetableBox = 'TimetableBox';
+  static const SubjectBox = 'SubjectBox';
+  static const TeacherBox = 'TeacherBox';
 }
