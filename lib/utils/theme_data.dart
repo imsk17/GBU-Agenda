1cb4c88a2c2f03422e5aba1ce394cc99c1761d0e
imsk17 <imsumitkumar17@gmail.com>
2020-12-06 18:39:41 +0530

    :bento: Add Assets And Fonts For UI

---
+++ lib/utils/theme_data.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+final theme = ThemeData.dark().copyWith(
+  accentColor: Color(0xFF36B6DE),
+  scaffoldBackgroundColor: Color(0xFF1C1F3E),
+  splashColor: Colors.white,
+  buttonTheme: ButtonThemeData(
+    splashColor: Colors.white,
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    buttonColor: Color(0xFF36B6DE),
+    focusColor: Color(0xFF36B6DE),
+  ),
+  textTheme: TextTheme(
+    headline1: TextStyle(
+        fontSize: 64,
+        color: Colors.white,
+        fontFamily: 'NeutrifPro',
+        fontWeight: FontWeight.w500),
+    headline2:
+        TextStyle(fontSize: 55, color: Colors.white, fontFamily: 'NeutrifPro'),
+    headline3:
+        TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'TTCommons'),
+    headline4:
+        TextStyle(fontSize: 18, color: Colors.white, fontFamily: 'TTCommons'),
+    headline5:
+        TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'ApercuPro'),
+    headline6:
+        TextStyle(fontSize: 18, color: Colors.white, fontFamily: 'ApercuPro'),
+  ),
+  iconTheme: IconThemeData(color: Colors.white, size: 24),
+);
